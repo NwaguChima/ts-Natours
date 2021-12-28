d4b31ed40a8afcb3940fcd1f7de6923bb795d9be
nwagu chima <chima.nwagu@decagon.dev>
2021-12-28 20:30:39 +0100

    implementing simple get and post routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,61 @@
+import fs from 'fs';
 import express, { Application, Request, Response, NextFunction } from 'express';
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('Hello from the server side !');
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req: Request, res: Response) => {
+  const id = +req.params.id;
+
+  const tour = tours.find((el: any) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req: Request, res: Response) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port: number = 3000;
